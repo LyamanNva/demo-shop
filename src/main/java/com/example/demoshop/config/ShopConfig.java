8bb7ce7a9228005566bb10a586898fe67d27370a
LyamanNva <lemannuriyeva006@gmail.com>
2024-09-11 22:26:45 +0400

    Initial commit

---
+++ src/main/java/com/example/demoshop/config/ShopConfig.java
@@ -0,0 +1,4 @@
+package com.example.demoshop.config;
+
+public class ShopConfig {
+}
---
+++ src/main/java/com/example/demoshop/request/ProductUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.example.demoshop.request;
+
+import com.example.demoshop.model.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class ProductUpdateRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
+}
